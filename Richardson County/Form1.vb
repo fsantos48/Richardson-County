a1e8758a5f39419578fcbd359147dd7522710612
FSC <santosarenillas@hotmail.com>
2016-11-06 20:35:02 +0100

    Commit1

--- Richardson County/Form1.vb
+++ Richardson County/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Impuesto As Decimal
+        Impuesto = CDec(TextBox1.Text) * 0.05
+        Label4.Text = Impuesto
 
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = ""
+        Label4.Text = ""
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+
+    End Sub
 End Class
